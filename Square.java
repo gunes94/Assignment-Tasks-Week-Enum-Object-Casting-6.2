632de4a8096494a78342dc9d36acf8e768a832e6
Gyunesh Mehmed <30699247+gunes94@users.noreply.github.com>
2024-01-12 18:55:05 +0200

    Create Square.java

---
+++ Square.java
@@ -0,0 +1,25 @@
+public class Square extends Shape{
+  private double side;
+
+  public Square(double side) {
+    this.side = side;
+  }
+
+  public double getSide() {
+    return side;
+  }
+
+  public void setSide(double side) {
+    this.side = side;
+  }
+
+  @Override
+  void getArea() {
+    System.out.printf("Square`s area: %.2f cm.\n", side * side );
+  }
+
+  @Override
+  void getPerimeter() {
+    System.out.printf("Square`s perimeter: %.2f cm.\n", 4 * side);
+  }
+}

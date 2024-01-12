8f2ce45c7a92e3f5357c502dfe68e982fe272f21
Gyunesh Mehmed <30699247+gunes94@users.noreply.github.com>
2024-01-12 18:54:42 +0200

    Create ShapeTest.java

---
+++ ShapeTest.java
@@ -0,0 +1,14 @@
+public class ShapeTest {
+
+  public static void main(String[] args) {
+    Circle circle = new Circle(5);
+    Square square = new Square(7);
+
+    circle.getArea();
+    circle.getPerimeter();
+
+    square.getArea();
+    square.getPerimeter();
+  }
+
+}
